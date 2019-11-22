5b8197f8704c22daba699f951313ad2a30927268
Ashwani Jha <ashwanijha04@gmail.com>
2019-11-22 16:50:04 +0530

    Update test_branch.sh

--- test_branch.sh
+++ test_branch.sh
@@ -5,7 +5,7 @@ if [ $BRANCH == 'master' ]
 then
   echo 'Cool! In code, we trust!'
 # Publish notification directly from the CLI. Use Branch name in the message.
-  aws sns publish --topic-arn "arn:aws:sns:us-west-2:0123456789012:my-topic" --message <string>
+   echo 'aws sns publish --topic-arn "arn:aws:sns:us-west-2:0123456789012:my-topic" --message <string>'
 else
   echo 'Sorry, this code doesnt have what it takes. Fail Build.'
 fi
