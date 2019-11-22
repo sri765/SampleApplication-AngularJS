2e5988892b85d3f2f62bf31e4ab34619517fc202
Ashwani Jha <ashwanijha04@gmail.com>
2019-11-22 15:46:28 +0530

    Update test_branch.sh

--- test_branch.sh
+++ test_branch.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-if [ $(echo $CODEBUILD_WEBHOOK_HEAD_REF) == 'refs/heads/master' ]
-then
-  echo 'Cool! In code, we trust!'
-  aws sns publish --topic-arn "arn:aws:sns:us-west-2:0123456789012:my-topic" --message <string>
-else
-  echo 'Sorry, this code doesn't have what it takes. Fail Build.'
-fi
+# git branch -a --contains HEAD| sed -n 2p | awk '{printf $1}' | rev | cut -d '/' -f 1 | rev
+
+echo $CODEBUILD_WEBHOOK_HEAD_REF | sed -n 2p | awk '{printf $1}' | rev | cut -d '/' -f 1 | rev
+
+# if [ $(echo $CODEBUILD_WEBHOOK_HEAD_REF) == 'refs/heads/master' ]
+# then
+#   echo 'Cool! In code, we trust!'
+#   aws sns publish --topic-arn "arn:aws:sns:us-west-2:0123456789012:my-topic" --message <string>
+# else
+#   echo 'Sorry, this code doesn't have what it takes. Fail Build.'
+# fi
 
