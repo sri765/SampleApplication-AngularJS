c9ca79a5d7b139a11cf45b5109dcce5a99a82bbb
Ashwani Jha <ashwanijha04@gmail.com>
2019-11-22 16:41:56 +0530

    Update test_branch.sh

--- test_branch.sh
+++ test_branch.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-# git branch -a --contains HEAD| sed -n 2p | awk '{printf $1}' | rev | cut -d '/' -f 1 | rev
 
-echo $CODEBUILD_WEBHOOK_HEAD_REF | sed -n 2p | awk '{printf $1}' | rev | cut -d '/' -f 1 | rev
+export BRANCH=$(echo $CODEBUILD_WEBHOOK_HEAD_REF | rev | cut -d '/' -f 1 | awk '{printf $1}' | rev)
+echo $BRANCH
 
-# if [ $(echo $CODEBUILD_WEBHOOK_HEAD_REF) == 'refs/heads/master' ]
-# then
-#   echo 'Cool! In code, we trust!'
-#   aws sns publish --topic-arn "arn:aws:sns:us-west-2:0123456789012:my-topic" --message <string>
-# else
-#   echo 'Sorry, this code doesn't have what it takes. Fail Build.'
-# fi
+if [ $BRANCH == 'master' ]
+then
+  echo 'Cool! In code, we trust!''
 
+# Publish notification directly from the CLI. Use Branch name in the message.
+  aws sns publish --topic-arn "arn:aws:sns:us-west-2:0123456789012:my-topic" --message <string>
+
+else
+  echo 'Sorry, this code doesn't have what it takes. Fail Build.'
+fi
